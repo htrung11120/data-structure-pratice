d6f24fe8edd854cb0657a60025bce0424cb48e48
htrung11120 <143675045+htrung11120@users.noreply.github.com>
2023-10-03 16:03:36 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,132 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = new Node(value);
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) return undefined
+    let temp = this.head;
+    let pre = this.head;
+    while (temp.next) {
+      pre = temp;
+      temp = temp.next
+    }
+    this.tail = pre;
+    this.tail.next = null;
+    this.length--
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return this
+  }
+  unshift(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++
+    return this
+  }
+  shift() {
+    if (!this.head) return undefined;
+    const temp = this.head;
+    this.head = this.head.next;
+    temp.next = null;
+    this.length--
+    if (this.length === 0) {
+      this.head = null
+      this.tail = null
+    }
+    return temp
+  }
+  get(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    let temp = this.head;
+    for (let i = 0; i < index; i++) {
+      temp = temp.next;
+    }
+    return temp;
+  }
+  set(index, value) {
+    let temp = this.get(index);
+    if (temp) {
+      temp.value = value;
+      return true;
+    }
+    return false;
+  }
+  insert(value, index) {
+    if (index === 0) return this.shift(value)
+    if (index === this.length) return this.push(value)
+    if (index < 0 || index > this.length) return false;
+    const newNode = new Node(value);
+    let temp = this.get(index - 1);
+    temp.next = newNode;
+    newNode.next = temp.next;
+    this.length++;
+    return true
+  }
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+
+    let before = this.get(index - 1);
+    let temp = before.next;
+
+    before.next = temp.next;
+    temp.next = null;
+    this.length--;
+    return temp;
+  }
+  reverse() {
+    let temp = this.head;
+    this.head = this.tail;
+    this.tail = temp;
+    let next = temp.next; 
+    let prev = null
+    for (let i = 0; i < this.length; i++) {
+      next = temp.next;
+      temp.next=prev;
+      prev=temp;
+      temp=next;
+    }
+  }
+
+}
+
+let myList = new LinkedList(1)
+myList.push(2)
+myList.push(3)
+myList.pop()
+myList.reverse()
+
+
+console.log(myList)
+
